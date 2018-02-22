4c59612df80cb73a1b35da69837407a611cdc780
bobbylinux <roberto.bani@gmail.com>
2018-02-22 21:15:31 +0100

    creato filtro per informazioni del paziente nel service

--- app/Services/v1/PatientService.php
+++ app/Services/v1/PatientService.php
@@ -8,6 +8,29 @@ class PatientService
 {
     public function getPatients()
     {
-        return Patient::all();
+        return $this->filterPatients(Patient::all());
+    }
+
+    private function filterPatients($patients)
+    {
+        $data = array();
+
+        foreach ($patients as $patient) {
+            $birthday = date_create($patient->date_of_birth);
+
+            $item = array(
+                'id' => $patient->id,
+                'name' => $patient->first_name,
+                'surname' => $patient->last_name,
+                'tax_code' => $patient->tax_code,
+                'sex' => $patient->sex,
+                'birthday' => $birthday->format('d-m-Y'),
+                'city_of_birth' => $patient->city_of_birth
+            );
+
+            $data[]=$item;
+        }
+
+        return $data;
     }
 }
